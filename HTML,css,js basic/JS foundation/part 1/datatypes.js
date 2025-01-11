5b31cf4509ea8a3e5ffdcb61adaab5d158eaaf59
Ayushbansal946 <77878747+Ayushbansal946@users.noreply.github.com>
2025-01-11 19:07:02 +0530

    part 1 of js

---
+++ HTML,css,js basic/JS foundation/part 1/datatypes.js
@@ -0,0 +1,22 @@
+/* string / number / boolean /bigint / undefined / null / object / symbol / character / float
+*/
+let b="abc";
+let a=10;
+let c=true;
+let d=10n;
+let e=undefined;
+let f=null;
+let g={ name:"ayush bansal" , age:27 };// let g={ name:"ayush", age:27};
+let h=Symbol("abc");
+let i="a";
+let j=10.5;
+console.log(typeof b);
+console.log(typeof a);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
+console.log(typeof i);
+console.log(typeof j);
