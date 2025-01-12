073bf5558abbcb1c88d4a0fb1e2588cf6eed2282
Ayushbansal946 <77878747+Ayushbansal946@users.noreply.github.com>
2025-01-12 10:20:34 +0530

    Create valuechange.js

---
+++ HTML,css,js basic/JS foundation/part 1/valuechange.js
@@ -0,0 +1,11 @@
+var a=10;
+let b=20;
+const c=30;
+console.log(a); //10
+console.log(b); //20
+console.log(c); //30
+a=100;
+b=200;
+console.log(a); //100
+console.log(b); //200
+// c=300; //error because we cannot change the value of constant variable
