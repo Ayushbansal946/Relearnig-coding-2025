0c11cbf07ae06f4919e2508c45e4a9d60ec0858a
Ayushbansal946 <77878747+Ayushbansal946@users.noreply.github.com>
2025-01-12 11:37:00 +0530

    Create logical.js

---
+++ HTML,css,js basic/JS foundation/part 1/logical.js
@@ -0,0 +1,12 @@
+// && = And
+// || = Or
+// ! = Not
+// Logical operators are used to determine the logic between variables or values.
+// Given the following variables:
+let a = true;
+let b = false;  
+let c = true;
+// What will the following statement return?
+console.log(a && b); //it will return false because both the values are not true 
+console.log(a || b); //it will return true because one of the value is true
+console.log(!a); //it will return false because the value of a is true and ! is used to reverse the value
