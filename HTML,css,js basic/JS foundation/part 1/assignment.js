d5b63046fe51b34e901c91e73ac33268e546ab14
Ayushbansal946 <77878747+Ayushbansal946@users.noreply.github.com>
2025-01-12 11:40:34 +0530

    Create assignment.js

---
+++ HTML,css,js basic/JS foundation/part 1/assignment.js
@@ -0,0 +1,21 @@
+let sum=1+2;// = is used to assign the value to the variable( sum in this case)
+console.log(sum);
+//but you can also use other operator to assign the value to the variable like
+let num1=1;
+num1+=2;// num1=num1+2;
+console.log(num1);
+let num2=1;
+num2-=2;// num2=num2-2;
+console.log(num2);
+let num3=1;
+num3*=2;// num3=num3*2;
+console.log(num3);
+let num4=1;
+num4/=2;// num4=num4/2;
+console.log(num4);
+let num5=1;
+num5%=2;// num5=num5%2;
+console.log(num5);
+let num6=1;
+num6**=2;// num6=num6**2;
+console.log(num6);
