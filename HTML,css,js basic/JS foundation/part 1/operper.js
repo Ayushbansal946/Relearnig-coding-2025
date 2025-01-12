ea88679e0bd2cc78d7d14228d8c80e466473157a
Ayushbansal946 <77878747+Ayushbansal946@users.noreply.github.com>
2025-01-12 11:47:07 +0530

    Update operper.js

--- HTML,css,js basic/JS foundation/part 1/operper.js
+++ HTML,css,js basic/JS foundation/part 1/operper.js
@@ -2,3 +2,10 @@
 // 1. () 2. ** 3. * / % 4. + - 5. >> >>> << 6. < > <= >= 7. == != 8. & 9. ^ 10. | 11. && 12. || 13. ?:
 // operator associativity
 // left to right
+// to aboid the precedence and associativity we can use () to make the code more readable like
+let a = 1 + 2 * 3;
+console.log(a); //7 // in this case the * will be executed first and then + will be executed
+let b = (1 + 2) * 3;
+console.log(b); //9 // in this case the () makes the code more readable and it also makes the code more understandable
+let c = ((1 + (2 * 3)) / 2);
+console.log(c); //4  in this case the () makes the code more readable and it also makes the code more understandable
