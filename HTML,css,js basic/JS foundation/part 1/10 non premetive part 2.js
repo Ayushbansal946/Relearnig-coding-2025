7f15aef9726f942b876a05254de143f79ab05b93
Ayushbansal946 <ayushbansal946@gmail.com>
2025-01-13 18:35:03 +0530

    Update 10 non premetive part 2.js

--- HTML,css,js basic/JS foundation/part 1/10 non premetive part 2.js
+++ HTML,css,js basic/JS foundation/part 1/10 non premetive part 2.js
@@ -4,3 +4,7 @@ console.log(arr);// [ 1, 2, 3, 4, 5 ]
 
 let arr2=[1,2,3,4,5,"abc",true];// array can store multiple data types
 console.log(arr2);// [ 1, 2, 3, 4, 5, 'abc', true ]
+
+//type conversion
+let islogin=true;// boolean
+console.log(1+islogin);// 2 // true=1 false=0 in javascript
