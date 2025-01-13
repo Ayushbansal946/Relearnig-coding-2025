600492ede13bf554fe97184ea2bcc5306c710b79
Ayushbansal946 <ayushbansal946@gmail.com>
2025-01-13 19:55:13 +0530

    Update 11 conditional.js

--- HTML,css,js basic/JS foundation/part 1/11 conditional.js
+++ HTML,css,js basic/JS foundation/part 1/11 conditional.js
@@ -10,6 +10,19 @@ else {
   console.log("you can't drive");
 }
 
+let num1=10;
+if(num1<10){
+  console.log("num1 is less than 10");
+}
+else if(num1>20){
+  console.log("num1 is greater than 20");
+}
+else if(num1==10){
+  console.log("num1 is equal to 10");
+}
+else{
+console.log("num1 is between 10 and 20");
+}
 //switch statement
 let day = 2;
 switch (day) {
