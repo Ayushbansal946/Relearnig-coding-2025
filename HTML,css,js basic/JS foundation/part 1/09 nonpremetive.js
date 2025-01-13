f29d2d2ca2afbba3971671bde18a0816b3fc40d8
Ayushbansal946 <77878747+Ayushbansal946@users.noreply.github.com>
2025-01-13 15:30:39 +0530

    Update 09 nonpremetive.js

--- HTML,css,js basic/JS foundation/part 1/09 nonpremetive.js
+++ HTML,css,js basic/JS foundation/part 1/09 nonpremetive.js
@@ -4,14 +4,23 @@ console.log(obj1);// {}
 
 
 let username={
-  fname:"ayush",
+  "f name":"ayush",
   lname:"bansal",
   islogin:true
 };// object
 console.log(username);// { fname: 'ayush', lname: 'bansal', islogin: true }
 
+// how to axcess object value
+console.log(username.lname);// bansal
+console.log(username["f name"]);// f name is a key so we can't use . to axcess it we have to use [] to axcess it
 
-
-
+const user1={
+  username:"ayush",
+  email:"ayushbansal946@gmail.com",
+  password:"123456"
+  }
+user1.username="ayush2";// we can change the value of object key even if it is constant variable
+user1.islogin=true;// we can add new key value pair in object even if it is constant variable
+console.log(user1);// { username: 'ayush2', email: 'ayushbansal946@gmail.com', password: '123456', islogin: true }
 
 let obj2=new Object();// object
