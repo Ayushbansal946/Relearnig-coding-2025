f4d1c36d619f76774d67cdd8fd92756acad7cbec
Ayushbansal946 <ayushbansal946@gmail.com>
2025-01-13 19:06:42 +0530

    Create 11 conditional.js

---
+++ HTML,css,js basic/JS foundation/part 1/11 conditional.js
@@ -0,0 +1,41 @@
+//conditional statements
+//if else statement , switch statement, ternary operator, short circuit operator
+
+//if else statement
+let age = 18;
+if (age >= 18) {
+  console.log("you can drive");
+}
+else {
+  console.log("you can't drive");
+}
+
+//switch statement
+let day = 2;
+switch (day) {
+
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+    console.log("wednesday");
+    break;
+  case 4:
+    console.log("thursday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  case 6:
+    console.log("saturday");
+    break;
+  case 7:
+    console.log("sunday");
+    break;
+  default:
+    console.log("invalid day");
+    break;
+}
