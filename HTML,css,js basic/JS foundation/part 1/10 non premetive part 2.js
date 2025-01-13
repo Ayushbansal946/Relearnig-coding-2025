205e0f66750595604efbdd5cfd8134840cb232d1
Ayushbansal946 <ayushbansal946@gmail.com>
2025-01-13 18:21:24 +0530

    Update 10 non premetive part 2.js

--- HTML,css,js basic/JS foundation/part 1/10 non premetive part 2.js
+++ HTML,css,js basic/JS foundation/part 1/10 non premetive part 2.js
@@ -1,3 +1,6 @@
 //non premetive data type part 2
 let arr=[1,2,3,4,5];// array
 console.log(arr);// [ 1, 2, 3, 4, 5 ]
+
+let arr2=[1,2,3,4,5,"abc",true];// array can store multiple data types
+console.log(arr2);// [ 1, 2, 3, 4, 5, 'abc', true ]
