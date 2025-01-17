974aab6cd200568986442b1784101417fcffbced
Ayushbansal946 <ayushbansal946@gmail.com>
2025-01-17 12:29:47 +0530

    Update 05 functions.js

---
+++ HTML,css,js basic/JS foundation/part 2/05 functions.js
@@ -0,0 +1,9 @@
+// function in js
+// function is a block of code that can be called and executed multiple times
+
+function ordertea(teatype)
+{
+return ` order confirmed for ${teatype}`; // return is used to return the value of the function
+}
+
+console.log(ordertea("ice tea"));// order confirmed for ice tea
