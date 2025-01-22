b3a5c49d9c958e39cf05761afce6aa83db62af38
Ayushbansal946 <ayushbansal946@gmail.com>
2025-01-22 15:00:12 +0530

    Update 02 custrocter function.js

--- HTML,css,js basic/java script/Js opps/02 custrocter function.js
+++ HTML,css,js basic/java script/Js opps/02 custrocter function.js
@@ -2,8 +2,11 @@
 function Person(name, age) {
   this.name = name;
   this.age = age;
-  this.greet = function () {
-    console.log(`Hello ${this.name}`);
-  };
 }
+
+Person.prototype.greet = function () {
+  
+  return `Hello ${this.name} and my age is ${this.age}`;
+};
 const person1 = new Person("ayush", 27);
+console.log(`person1`, person1.greet());
