70cc9df370d50bb015745fd0aec52a8668916865
Ayushbansal946 <ayushbansal946@gmail.com>
2025-01-28 19:15:26 +0530

    separating js from html

---
+++ HTML,css,js basic/java script/mini projects/2025 remake of (solve 2017)/Virsion 1.1/scripts/Script8.js
@@ -0,0 +1,118 @@
+
+function sw1()
+{
+{
+var a,b,c;
+a=document.getElementById("i1").src;
+b=document.getElementById("i5").src;
+c=a;
+a=b;
+b=c;
+document.getElementById("i1").src=a;
+document.getElementById("i5").src=b;
+}
+}
+function sw2()
+{
+{
+var a,b,c;
+a=document.getElementById("i2").src;
+b=document.getElementById("i1").src;
+c=a;
+a=b;
+b=c;
+document.getElementById("i2").src=a;
+document.getElementById("i1").src=b;
+}
+}
+function sw3()
+{
+{
+var a,b,c;
+a=document.getElementById("i3").src;
+b=document.getElementById("i8").src;
+c=a;
+a=b;
+b=c;
+document.getElementById("i3").src=a;
+document.getElementById("i8").src=b;
+}
+}
+function sw4()
+{
+{
+var a,b,c;
+a=document.getElementById("i4").src;
+b=document.getElementById("i3").src;
+c=a;
+a=b;
+b=c;
+document.getElementById("i4").src=a;
+document.getElementById("i3").src=b;
+}
+}
+function sw5()
+{
+{
+var a,b,c;
+a=document.getElementById("i5").src;
+b=document.getElementById("i4").src;
+c=a;
+a=b;
+b=c;
+document.getElementById("i5").src=a;
+document.getElementById("i4").src=b;
+}
+}
+function sw6()
+{
+{
+var a,b,c;
+a=document.getElementById("i6").src;
+b=document.getElementById("i2").src;
+c=a;
+a=b;
+b=c;
+document.getElementById("i6").src=a;
+document.getElementById("i2").src=b;
+}
+}
+function sw7()
+{
+{
+var a,b,c;
+a=document.getElementById("i7").src;
+b=document.getElementById("i9").src;
+c=a;
+a=b;
+b=c;
+document.getElementById("i7").src=a;
+document.getElementById("i9").src=b;
+}
+}
+function sw8()
+{
+{
+var a,b,c;
+a=document.getElementById("i8").src;
+b=document.getElementById("i7").src;
+c=a;
+a=b;
+b=c;
+document.getElementById("i8").src=a;
+document.getElementById("i7").src=b;
+}
+}
+function sw9()
+{
+{
+var a,b,c;
+a=document.getElementById("i9").src;
+b=document.getElementById("i6").src;
+c=a;
+a=b;
+b=c;
+document.getElementById("i9").src=a;
+document.getElementById("i6").src=b;
+}
+}
