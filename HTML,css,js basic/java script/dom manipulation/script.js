78b2bc86ce2713c05a875b8854b87d5a05ec5f57
Ayushbansal946 <ayushbansal946@gmail.com>
2025-01-29 19:58:55 +0530

    .

--- HTML,css,js basic/java script/dom manipulation/script.js
+++ HTML,css,js basic/java script/dom manipulation/script.js
@@ -1,5 +1,5 @@
 //example 1
-
+//there are diffrent metthods to select the element in the dom (document object model) or bom (browser object model) and javascript all three are a part of the window object
 document
   .getElementById("changeTextButton")
   .addEventListener("click", function () {
