3daac710b497ff0e994b51d14eebae60d3dccdd1
Ayushbansal946 <ayushbansal946@gmail.com>
2025-01-31 19:57:56 +0530

    dom manipulation

--- HTML,css,js basic/java script/dom manipulation/script.js
+++ HTML,css,js basic/java script/dom manipulation/script.js
@@ -1,8 +1,22 @@
-//example 1
 //there are diffrent metthods to select the element in the dom (document object model) or bom (browser object model) and javascript all three are a part of the window object
+//example 1
 document
   .getElementById("changeTextButton")
   .addEventListener("click", function () {
     let paragraph = document.getElementById("myParagraph");
     paragraph.textContent = "the paragraph is changed";
   });
+//example 2
+  document.getElementById("highlightFirstCity").addEventListener("click", function(){
+   document.getElementById("citiesList").firstElementChild.classList.add("highlight");
+  })
+  //example 3
+  document.getElementById('changeOrder').addEventListener('click', function(){
+    document.getElementById('coffeeType').textContent = 'espresso'; 
+  })
+//example 4
+  document.getElementById('addNewItem').addEventListener('click', function(){
+let newItem = document.createElement('li');
+newItem.textContent = 'chocolate';
+document.getElementById("shoppingList").appendChild(newItem);
+})
