901771f0a56af61796cdddcbf539f05abb659bb2
Ayushbansal946 <ayushbansal946@gmail.com>
2025-02-06 15:45:44 +0530

    JS code improvement in mini project

---
+++ HTML,css,js basic/java script/mini projects/2025 remake of (solve 2017)/Virsion 1.2/scripts/Script1.js
@@ -0,0 +1,33 @@
+function swapElements(id1, id2) {
+  var temp = document.getElementById(id1).src;
+  document.getElementById(id1).src = document.getElementById(id2).src;
+  document.getElementById(id2).src = temp;
+}
+
+function sw1() {
+  swapElements("i1", "i2");
+}
+function sw2() {
+  swapElements("i2", "i3");
+}
+function sw3() {
+  swapElements("i3", "i6");
+}
+function sw4() {
+  swapElements("i4", "i7");
+}
+function sw5() {
+  swapElements("i5", "i4");
+}
+function sw6() {
+  swapElements("i6", "i5");
+}
+function sw7() {
+  swapElements("i7", "i8");
+}
+function sw8() {
+  swapElements("i8", "i9");
+}
+function sw9() {
+  swapElements("i9", "i1");
+}
