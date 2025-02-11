c3fd24f8d0e86d57c92c71df5cde0503ded362d8
Ayushbansal946 <ayushbansal946@gmail.com>
2025-02-11 15:21:48 +0530

    Update 02 promises.js

--- HTML,css,js basic/java script/js adv/02 promises.js
+++ HTML,css,js basic/java script/js adv/02 promises.js
@@ -1,5 +1,29 @@
 //promises are used to handle asynchronous operations in javascript
 //promises are the better way to handle asynchronous operations in javascript
-// promises helps us to achive asencronous programming in javascript 
+// promises helps us to achive asencronous programming in javascript we can also use (async and await for this purpose)
 
+function fetchdata()
+{
+  return new Promise((resolve, reject)=>{
+setTimeout(() => {
+  let success=true;
+  if(success)
+  {
+    resolve("Data Fetched Successfully");//if we use resolve then then block will be executed we can send any data to then block  like array object etc
+  }
+  else
+  {
+    reject("data not fetched");//if we use reject then catch block will be executed
+  }
+}, 2000);
 
+  })
+}
+fetchdata()
+  .then((msg)=>{//callback function of then block will be executed if the promise is resolved
+    console.log(msg);
+    let data=msg.toLowerCase();
+    return data;//we can return any data from then block and it will be passed to the next then block
+  }).then((value)=>console.log(value))//we can use then chain to handle the promisefurther
+  .catch((msg)=>console.log(msg)//callback function of catch block will be executed if the promise is rejected 
+  );//we can use then and catch block to handle the promise 
