a3686f7b4e359245a1805c344c34857ac470b739
Ayushbansal946 <ayushbansal946@gmail.com>
2025-02-13 18:30:29 +0530

    Update script.js

--- HTML,css,js basic/java script/mini projects/todolist app/script.js
+++ HTML,css,js basic/java script/mini projects/todolist app/script.js
@@ -1,1 +1,26 @@
-alert("hello");
+document.addEventListener('DOMContentLoaded', ()=> {
+const input = document.getElementById("todo-input")
+const addtskBtn = document.getElementById("add-task-btn")
+const todoList = document.getElementById("todo-list")
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+tasks.forEach(task => renderTask(task));
+addtskBtn.addEventListener("click", () => {
+  if (input.value==="") return;
+    const tasktext=input.value.trim();
+    const task = {
+    id:Date.now(),
+    text:tasktext,
+    completed:false,
+    };
+    tasks.push(task);
+    savedTasks();
+    input.value = "";
+    console.log(tasks);
+});
+function renderTask(task){
+
+}
+function savedTasks(){
+  localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+});
