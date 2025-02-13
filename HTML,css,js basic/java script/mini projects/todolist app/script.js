1f49d4b6a81d938736b905046abb4296b73cf77d
Ayushbansal946 <ayushbansal946@gmail.com>
2025-02-13 19:25:39 +0530

    Update script.js

--- HTML,css,js basic/java script/mini projects/todolist app/script.js
+++ HTML,css,js basic/java script/mini projects/todolist app/script.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-const input = document.getElementById("todo-input")
-const addtskBtn = document.getElementById("add-task-btn")
-const todoList = document.getElementById("todo-list")
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-tasks.forEach(task => renderTask(task));
+  const input = document.getElementById("todo-input")
+  const addtskBtn = document.getElementById("add-task-btn")
+  const todoList = document.getElementById("todo-list")
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  
+  tasks.forEach(task => renderTask(task));
+  
+  function renderTask(task){
+  const li = document.createElement("li");
+  li.innerHTML =`
+  <span>${task.text}</span>
+  <button class="li-btn" data-id="${task.id}">Delete</button>
+  `
+  todoList.appendChild(li);
+  }
+
 addtskBtn.addEventListener("click", () => {
   if (input.value==="") return;
     const tasktext=input.value.trim();
@@ -17,10 +28,10 @@ addtskBtn.addEventListener("click", () => {
     input.value = "";
     console.log(tasks);
 });
-function renderTask(task){
 
-}
+
 function savedTasks(){
   localStorage.setItem("tasks",JSON.stringify(tasks));
+  renderTask(tasks[tasks.length-1]);
 }
 });
