b0745b1021abbf9dff5b8a5e9894181ce76b170f
Ayushbansal946 <ayushbansal946@gmail.com>
2025-02-16 16:37:18 +0530

    Update script.js

--- HTML,css,js basic/java script/mini projects/todolist app/script.js
+++ HTML,css,js basic/java script/mini projects/todolist app/script.js
@@ -2,10 +2,32 @@ document.addEventListener('DOMContentLoaded', ()=> {
   const input = document.getElementById("todo-input")
   const addtskBtn = document.getElementById("add-task-btn")
   const todoList = document.getElementById("todo-list")
+
+
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  //rendering the tasks from the local storage
+  tasks.forEach(task => renderTask(task));  
   
-  tasks.forEach(task => renderTask(task));
-  
+  addtskBtn.addEventListener("click", () => {
+  if (input.value==="") return;
+    const tasktext=input.value.trim();//.trim() removes the white spaces 
+    //task creation
+    const task = {
+    id:Date.now(),
+    text:tasktext,
+    completed:false,
+    };
+    //pushing the task to the tasks array
+    tasks.push(task);
+    savedTasks();
+    input.value = "";//clear the input field
+    //console.log(tasks);
+})
+  function savedTasks(){
+  localStorage.setItem("tasks",JSON.stringify(tasks));
+  renderTask(tasks[tasks.length-1]);
+}
+
   function renderTask(task){
   const li = document.createElement("li");
   li.innerHTML =`<span>${task.text}</span>
@@ -13,23 +35,4 @@ document.addEventListener('DOMContentLoaded', ()=> {
   todoList.appendChild(li);
   }
 
-addtskBtn.addEventListener("click", () => {
-  if (input.value==="") return;
-    const tasktext=input.value.trim();//.trim() removes the white spaces 
-    const task = {
-    id:Date.now(),
-    text:tasktext,
-    completed:false,
-    };
-    tasks.push(task);
-    savedTasks();
-    input.value = "";
-    console.log(tasks);
-});
-
-
-function savedTasks(){
-  localStorage.setItem("tasks",JSON.stringify(tasks));
-  renderTask(tasks[tasks.length-1]);
-}
 });
