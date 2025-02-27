7a7440e7b3236dd0b4da04acb2f3cc080420bf9c
Ayushbansal946 <ayushbansal946@gmail.com>
2025-02-27 16:40:14 +0530

    Update script.js

---
+++ HTML,css,js basic/java script/mini projects/03_ecommerce/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const products= 
+[  {id:1,name:"product1",price:99},
+  {id:2,name:"product2 ",price:250},
+  {id:3,name:"product3",price:499}];//products array
+  const cart=[];    //products will be pushed here
+const productList=document.getElementById("product-list");
+const cartItems=document.getElementById("cart-items");
+const emptyCartMsg=document.getElementById("empty-cart");
+const cartTotalMsg=document.getElementById("cart-total");
+const totalPriceDisplay=document.getElementById("total-price");
+const checkOutBtn=document.getElementById("checkout-btn");
+products  .forEach((product)=>{
+  const productDiv=document.createElement("div");
+  productDiv.classList.add("product");
+  productDiv.innerHTML=`
+  <span>${product.name} - $${product.price.toFixed(2)}</span>
+  <button data-id="${product.id}">Add to Cart</button>
+  `
+});
+
+});
