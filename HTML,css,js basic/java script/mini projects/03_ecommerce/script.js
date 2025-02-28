0b0f942ec93648627e813d6b50cd7e52f231169a
Ayushbansal946 <ayushbansal946@gmail.com>
2025-02-28 10:02:51 +0530

    Update script.js

--- HTML,css,js basic/java script/mini projects/03_ecommerce/script.js
+++ HTML,css,js basic/java script/mini projects/03_ecommerce/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const products= 
-[  {id:1,name:"product1",price:99},
-  {id:2,name:"product2 ",price:250},
+[ {id:1,name:"product1",price:99},
+  {id:2,name:"product2",price:250},
   {id:3,name:"product3",price:499}];//products array
   const cart=[];    //products will be pushed here
 const productList=document.getElementById("product-list");
@@ -10,13 +10,25 @@ const emptyCartMsg=document.getElementById("empty-cart");
 const cartTotalMsg=document.getElementById("cart-total");
 const totalPriceDisplay=document.getElementById("total-price");
 const checkOutBtn=document.getElementById("checkout-btn");
-products  .forEach((product)=>{
+products.forEach((product) => {
   const productDiv=document.createElement("div");
   productDiv.classList.add("product");
   productDiv.innerHTML=`
   <span>${product.name} - $${product.price.toFixed(2)}</span>
   <button data-id="${product.id}">Add to Cart</button>
-  `
+  `;
+  productList.appendChild(productDiv);
 });
+productList.addEventListener("click",(e)=>{
+  if(e.target.tagName==="BUTTON"){
+    const productId=parseInt(e.target.getAttribute("data-id"));
+    const product=products.find((product)=>product.id===productId);
+    cart.push(product);
+    renderCart();
+  }
+});
+function renderCart(){
+   
+}
 
 });
