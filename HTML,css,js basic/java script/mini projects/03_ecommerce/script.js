0d750cb402a5effff28009c57df5a7995f7eae54
Ayushbansal946 <ayushbansal946@gmail.com>
2025-03-01 18:43:20 +0530

    install tailwind css

--- HTML,css,js basic/java script/mini projects/03_ecommerce/script.js
+++ HTML,css,js basic/java script/mini projects/03_ecommerce/script.js
@@ -1,15 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const products= 
-[ {id:1,name:"product1",price:99},
-  {id:2,name:"product2",price:250},
-  {id:3,name:"product3",price:499}];//products array
+  
+  const productList=document.getElementById("product-list");
+  const cartItems=document.getElementById("cart-items");
+  const emptyCartMsg=document.getElementById("empty-cart");
+  const cartTotalMsg=document.getElementById("cart-total");
+  const totalPriceDisplay=document.getElementById("total-price");
+  const checkOutBtn=document.getElementById("checkout-btn");
+
+  const products=[ 
+  {id:1,name:"product1",price:99,quantity:0},
+  {id:2,name:"product2",price:250,quantity:0},
+  {id:3,name:"product3",price:499,quantity:0}];//products array
+  
   const cart=[];    //products will be pushed here
-const productList=document.getElementById("product-list");
-const cartItems=document.getElementById("cart-items");
-const emptyCartMsg=document.getElementById("empty-cart");
-const cartTotalMsg=document.getElementById("cart-total");
-const totalPriceDisplay=document.getElementById("total-price");
-const checkOutBtn=document.getElementById("checkout-btn");
+
 products.forEach((product) => {
   const productDiv=document.createElement("div");
   productDiv.classList.add("product");
@@ -19,16 +23,49 @@ products.forEach((product) => {
   `;
   productList.appendChild(productDiv);
 });
+
+
 productList.addEventListener("click",(e)=>{
   if(e.target.tagName==="BUTTON"){
     const productId=parseInt(e.target.getAttribute("data-id"));
     const product=products.find((product)=>product.id===productId);
-    cart.push(product);
-    renderCart();
+    product.quantity++;
+    addtocart(product);
   }
 });
+
+function addtocart(product){
+  if (cart.find(p=>p.id===product.id)){}
+  else
+{
+  cart.push(product);
+}  
+  renderCart();
+}
+
+
 function renderCart(){
+  cartItems.innerText = "";
+  let totalamount=0;
+  if (cart.length>0){
+    emptyCartMsg.classList.add("hidden");
+    cartTotalMsg.classList.remove("hidden");
+    cart.forEach((item)=>{
+      totalamount+=(item.price*item.quantity);
+      const cartItem=document.createElement("div");  
+      cartItem.innerHTML=`
+      ${item.name} - $${item.price.toFixed(2)} x ${item.quantity}`;
+      cartItems.appendChild(cartItem);
+      totalPriceDisplay.textContent=`$${totalamount.toFixed(2)}`;
+    });
+    
+    
+    
    
+}
+else{
+  emptyCartMsg.classList.remove("hidden");
+}
 }
 
 });
