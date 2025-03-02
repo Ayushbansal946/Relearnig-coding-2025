4097a60f623c9d93ed271369e4d57c2a0e779fda
Ayushbansal946 <ayushbansal946@gmail.com>
2025-03-02 12:23:35 +0530

    added the delete from cart function

--- HTML,css,js basic/java script/mini projects/03_ecommerce/script.js
+++ HTML,css,js basic/java script/mini projects/03_ecommerce/script.js
@@ -12,7 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
   {id:2,name:"product2",price:250,quantity:0},
   {id:3,name:"product3",price:499,quantity:0}];//products array
   
-  const cart=[];    //products will be pushed here
+  let cart=JSON.parse(localStorage.getItem("Cart")) ||[];    //products will be pushed here
+  renderCart();
 
 products.forEach((product) => {
   const productDiv=document.createElement("div");
@@ -29,43 +30,67 @@ productList.addEventListener("click",(e)=>{
   if(e.target.tagName==="BUTTON"){
     const productId=parseInt(e.target.getAttribute("data-id"));
     const product=products.find((product)=>product.id===productId);
-    product.quantity++;
     addtocart(product);
   }
 });
 
 function addtocart(product){
-  if (cart.find(p=>p.id===product.id)){}
+  product.quantity++;
+  const temp =cart.find(p=>p.id===product.id);
+  if (temp){
+    temp.quantity++;
+    localStorage.setItem("Cart",JSON.stringify(cart));
+  }
   else
 {
-  cart.push(product);
+  const tempitem={...product};//destructuring the product object
+  cart.push(tempitem);
+  localStorage.setItem("Cart",JSON.stringify(cart));
 }  
   renderCart();
 }
 
 
 function renderCart(){
-  cartItems.innerText = "";
   let totalamount=0;
   if (cart.length>0){
+    cartItems.innerText = "";
     emptyCartMsg.classList.add("hidden");
     cartTotalMsg.classList.remove("hidden");
     cart.forEach((item)=>{
       totalamount+=(item.price*item.quantity);
       const cartItem=document.createElement("div");  
+      cartItem.classList.add("added-cart-item");
       cartItem.innerHTML=`
-      ${item.name} - $${item.price.toFixed(2)} x ${item.quantity}`;
+      ${item.name} - $${item.price.toFixed(2)} Quantity : ${item.quantity} <button data-id="${item.id}" class="delete-btn">Delete</button>`;
       cartItems.appendChild(cartItem);
       totalPriceDisplay.textContent=`$${totalamount.toFixed(2)}`;
-    });
-    
-    
-    
-   
-}
+    });}
 else{
+  cartItems.innerText = "Your cart is empty.";
+  totalPriceDisplay.textContent=`$0.00`;
+  cartTotalMsg.classList.add("hidden");
+
   emptyCartMsg.classList.remove("hidden");
 }
 }
-
+checkOutBtn.addEventListener("click",()=>{
+  cart.length=0;
+  cartTotalMsg.classList.add("hidden");
+  cartItems.innerText="Thank you for shopping with us";
+  localStorage.setItem("Cart",JSON.stringify(cart));
+  renderCart();
+});
+cartItems.addEventListener("click",(e)=>{
+  if(e.target.tagName==="BUTTON"){
+    e.stopPropagation();
+    const id=parseInt(e.target.getAttribute("data-id"));
+    deleteItem(id);
+  }
+});
+function deleteItem(id){
+  cart=cart.filter((item)=>item.id!==id);
+  renderCart();
+  localStorage.setItem("Cart",JSON.stringify(cart));
+  };
 });
