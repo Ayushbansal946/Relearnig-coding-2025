30d9bf6fe1127bd78e0f8ba1d7a0a7cbc9714543
Ayushbansal946 <ayushbansal946@gmail.com>
2025-04-16 13:14:47 +0530

    Update script.js

--- HTML,css,js basic/java script/mini projects/04_Expence tracker/script.js
+++ HTML,css,js basic/java script/mini projects/04_Expence tracker/script.js
@@ -4,19 +4,54 @@ const expenseName=document.getElementById('expense-name');
 const expenseAmount=document.getElementById('expense-amount');
 const expenseList=document.getElementById('expense-list');
 const totalAmountDisplay=document.getElementById('total-amount');
-let expenses=[];
-let totalAmount=Caltotal();
+let expenses = JSON.parse(localStorage.getItem('expenses'))||[];
+function calcTotal(){
+    renderExpenses();
+    return expenses.reduce((sum,expense) => sum+expense.amount,0);
+}
+let totalAmount=calcTotal();
+
 expenseForm.addEventListener('submit',(e)=>{
 e.preventDefault();
-let name=expenseName.Value;
-let amount=parseFloat(expenseAmount.Value);
-if(name!=="" && !isNaN(amount) && amount>0 )
+let name=expenseName.value.trim();
+let amount=parseFloat(expenseAmount.value);
+if(name!== "" && !isNaN(amount) && amount > 0 )
     {
-      
+        const newExpense={
+            id: Date.now(),
+            name,
+            amount
+        };
+        expenses.push(newExpense);
+        saveExpensesToLocal();
+        //clear input
+        expenseName.value="";
+        expenseAmount.value="";
+        updateTotal();
+        renderExpenses();
     }
 })
+function renderExpenses()
+{
+    expenseList.innerHTML="";
+    expenses.forEach(expense => {
+        let li=document.createElement('li');
+        li.innerHTML=`
+         ${expense.name} - $${expense.amount}
+        <button data-id="${expense.id}">Delete</button>
+        `
+        expenseList.appendChild(li);
+    });
+}
 
-
+function saveExpensesToLocal()
+{
+    localStorage.setItem('expenses',JSON.stringify(expenses))
+}
+function updateTotal(){
+    totalAmount=calcTotal();
+    totalAmountDisplay.textContent=totalAmount.toFixed(2);
+}
 
 
 
